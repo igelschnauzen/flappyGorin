81cace5a55684cd0286035d7479bc8aa3341c58d
krieg-dev <JustDimas1168@yandex.ru>
2020-04-27 15:17:45 +0300

    fixed death-bug, added notification about death

--- script.js
+++ script.js
@@ -37,15 +37,15 @@ var gap = 95;
 document.addEventListener("keydown", moveUp);
 
 function moveUp() {
- yPos -= 25;
+	yPos -= 25;
 }
 
 // Создание блоков
 var pipe = [];
 
 pipe[0] = {
- x : cvs.width,
- y : 0
+	x : cvs.width,
+	y : 0
 }
 
 var score = 0;
@@ -55,44 +55,45 @@ var yPos = 150;
 var grav = 1.5;
 
 function draw() {
- ctx.drawImage(bg, 0, 0);
+	ctx.drawImage(bg, 0, 0);
 
- for(var i = 0; i < pipe.length; i++) {
- ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y);
- ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
+	for(var i = 0; i < pipe.length; i++) {
+		ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y);
+		ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
 
- pipe[i].x--;
+		pipe[i].x--;
 
- if(pipe[i].x == 125) {
- pipe.push({
- x : cvs.width,
- y : Math.floor(Math.random() * pipeUp.height) - pipeUp.height
- });
- }
+		if(pipe[i].x == 125) {
+			pipe.push({
+				x : cvs.width,
+				y : Math.floor(Math.random() * pipeUp.height) - pipeUp.height
+			 });
+ 		}
 
- // Отслеживание прикосновений
- if(xPos + bird.width >= pipe[i].x
- && xPos <= pipe[i].x + pipeUp.width
- && (yPos <= pipe[i].y + pipeUp.height
- || yPos + bird.height >= pipe[i].y + pipeUp.height + gap) || yPos + bird.height >= cvs.height - fg.height) {
- location.reload(); // Перезагрузка страницы
- }
 
- if(pipe[i].x == 5) {
- score++;
- }
- }
+		if(xPos + bird.width >= pipe[i].x
+		&& xPos <= pipe[i].x + pipeUp.width
+		&& (yPos <= pipe[i].y + pipeUp.height
+		|| yPos + bird.height >= pipe[i].y + pipeUp.height + gap) || yPos + bird.height >= cvs.height - fg.height) {
+			alert('Смерть')
+			location.reload(); // Перезагрузка страницы
+		}
 
- ctx.drawImage(fg, 0, cvs.height - fg.height);
- ctx.drawImage(bird, xPos, yPos);
+		if(pipe[i].x == 5) {
+			score++;
+		}
+	}
 
- yPos += grav;
+	ctx.drawImage(fg, 0, cvs.height - fg.height);
+	ctx.drawImage(bird, xPos, yPos);
 
- ctx.fillStyle = "#000";
- ctx.font = "24px Verdana";
- ctx.fillText("Счет: " + score, 10, cvs.height - 20);
+	yPos += grav;
 
- requestAnimationFrame(draw);
+	ctx.fillStyle = "#000";
+	ctx.font = "24px Verdana";
+	ctx.fillText("Счет: " + score, 10, cvs.height - 20);
+
+	requestAnimationFrame(draw);
 }
 
 pipeBottom.onload = draw;
@@ -100,5 +101,4 @@ pipeBottom.onload = draw;
 function hack (value) {
 	score += value;
 }
- 
  
