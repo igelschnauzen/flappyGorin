fddcea2b5f35b3c8d6df9d3b9fc57807156f7074
pieceofwater <kriegg878@gmail.com>
2021-01-17 14:32:40 +0300

    update

--- script.js
+++ script.js
@@ -1,69 +1,58 @@
 let cvs = document.getElementById("canvas");
 let ctx = cvs.getContext("2d");
 
-let gorin = new Image();
-let bg = new Image();
-let pipeUp = new Image();
-let pipeBottom = new Image();
-let fg = new Image();
-
-//sounds
-let scoreSound = new Audio();
-let deathSound = new Audio();
-
-deathSound.src = "sound/death.mp3";
-
-//select level
-function setLevel(level) {
-	level = prompt('lvl? (1/2)', '');
-
-	if(level == '1') {
-		gorin.src = "textures/img/gorin.player.png";
-		bg.src = "textures/img/bg.png";
-		pipeUp.src = "textures/img/vilka.topenemy.png";
-		pipeBottom.src = "textures/img/taburetka.bottomenemy.png";
-	
-		scoreSound.src = "sound/a.mp3";
-	} else if(level == '2') {
-		gorin.src = "textures/img2/gorin.player.png";
-		bg.src = "textures/img2/bg2.jpg";
-		pipeUp.src = "textures/img2/lopata.topenemy.png";
-		pipeBottom.src = "textures/img2/dver.bottomenemy.png";
-	
-		scoreSound.src = "sound/a.mp3";
-	} else if(level == '666') {
-		gorin.src = "textures/sl/poroshenko.player.png";
-		bg.src = "textures/sl/bg.png";
-		pipeUp.src = "textures/sl/putin.topenemy.jpg";
-		pipeBottom.src = "textures/sl/zel.bottomenemy.jpg";
-	
-		scoreSound.src = "sound/b.mp3";
-	} else if(level === null || level === undefined || level === NaN){
-		alert('што');
-		location.reload();
-
-	} else if(level === ''){
-		alert('но тут же пусто'); 
-		location.reload();
-
-	} else {
-		alert('тебе сказали русским языком блять 1 или 2, какой ' + level + '?!');
-		location.reload();
+class Level {
+	constructor(levelNumber) {
+		this.gorin = new Image();
+		this.bg = new Image();
+		this.pipeUp = new Image();
+		this.pipeBottom = new Image();
+		this.fg = new Image();
 		
-	}
+		this.scoreSound = new Audio();
+		this.deathSound = new Audio();
+		
+		this.deathSound.src = "sound/death.mp3";
+		
+		if(levelNumber == '1') {
+			this.gorin.src = "textures/img/gorin.player.png";
+			this.bg.src = "textures/img/bg.png";
+			this.pipeUp.src = "textures/img/vilka.topenemy.png";
+			this.pipeBottom.src = "textures/img/taburetka.bottomenemy.png";
 	
+			this.scoreSound.src = "sound/a.mp3";
+		} else if(levelNumber == '2') {
+			this.gorin.src = "textures/img2/gorin.player.png";
+			this.bg.src = "textures/img2/bg2.jpg";
+			this.pipeUp.src = "textures/img2/lopata.topenemy.png";
+			this.pipeBottom.src = "textures/img2/dver.bottomenemy.png";
+	
+			this.scoreSound.src = "sound/a.mp3";
+		} else if(levelNumber == '666') {
+			this.gorin.src = "textures/sl/poroshenko.player.png";
+			this.bg.src = "textures/sl/bg.png";
+			this.pipeUp.src = "textures/sl/putin.topenemy.jpg";
+			this.pipeBottom.src = "textures/sl/zel.bottomenemy.jpg";
+	
+			this.scoreSound.src = "sound/b.mp3";
+		} else {
+			alert(levelNumber);
+			location.reload();
+		
+		}
+	}
 }
 
+let level = new Level(prompt('Level?', ''));
+
 function death() {
-	deathSound.play();
+	level.deathSound.play();
 	alert(`\nСмерть.` +  `\n Счет: ` + score);
 
 	location.reload();
 
 	return;
 }
-
-setLevel();
 
 let gap = 95;
 
@@ -101,38 +90,39 @@ let grav = 1.9;
 
 function draw() {
 
-	ctx.drawImage(bg, 0, 0);
+	ctx.drawImage(level.bg, 0, 0);
 
 	for(let i = 0; i < pipe.length; i++) {
-		ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y);
-		ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
+		ctx.drawImage(level.pipeUp, pipe[i].x, pipe[i].y);
+		ctx.drawImage(level.pipeBottom, pipe[i].x, pipe[i].y + level.pipeUp.height + gap);
 	
 		pipe[i].x--;
 	
 		if(pipe[i].x == 125) {
 			pipe.push({
 				x : cvs.width,
-				y : Math.floor(Math.random() * pipeUp.height) - pipeUp.height
-				});
-		 }
+				y : Math.floor(Math.random() * level.pipeUp.height) - level.pipeUp.height
+			});
+		}
 	
 		//death
-		if(xPos + gorin.width >= pipe[i].x
-		&& xPos <= pipe[i].x + pipeUp.width
-		&& (yPos <= pipe[i].y + pipeUp.height
-		|| yPos + gorin.height >= pipe[i].y + pipeUp.height + gap) || yPos + gorin.height >= cvs.height - fg.height) {
+		if(xPos + level.gorin.width >= pipe[i].x
+		&& xPos <= pipe[i].x + level.pipeUp.width
+		&& (yPos <= pipe[i].y + level.pipeUp.height
+		|| yPos + level.gorin.height >= pipe[i].y + level.pipeUp.height + gap) 
+		|| yPos + level.gorin.height >= cvs.height - level.fg.height) {
 			return death();
 		}
 	
 		//giving score
 		if(pipe[i].x == 5) {
 			score++;
-			scoreSound.play();
+			level.scoreSound.play();
 		}
 	}
 	
-	ctx.drawImage(fg, 0, cvs.height - fg.height);
-	ctx.drawImage(gorin, xPos, yPos);
+	ctx.drawImage(level.fg, 0, cvs.height - level.fg.height);
+	ctx.drawImage(level.gorin, xPos, yPos);
 	
 	yPos += grav; //the effect of gravity
 	
@@ -150,4 +140,4 @@ function hack (value) {
 	score += value;
 }
 
-pipeBottom.onload = draw;
+level.pipeBottom.onload = draw;
