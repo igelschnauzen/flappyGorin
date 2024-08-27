623d0089bbe2136e5ff93e5dc03fea57b2a1a7cb
igelschnauzen <kriegg878@gmail.com>
2024-08-27 10:00:36 +0200

    main menu script added

---
+++ menu.js
@@ -0,0 +1,18 @@
+"use strict";
+
+let selectedLevel;
+
+document.addEventListener("click", (event) => {
+    if(event.target.tagName.toLowerCase() == "button") {
+        document.querySelector("#canvas").setAttribute("level", event.target.id);
+        
+        document.querySelector(".placeholder").hidden = true;
+        document.querySelector("#canvas").hidden = false;
+
+        let game = document.createElement("script");
+        game.src = "game.js";
+        game.id="game";
+        game.defer = true;
+        document.head.append(game);
+    }
+});
